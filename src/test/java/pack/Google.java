f565aa77c3bf7a54e8c818aecd32e8ca1f0da33c
yaiphabakm <yaiphaba0012@gmail.com>
2016-10-19 20:11:25 +0530

    v 1.0

---
+++ src/test/java/pack/Google.java
@@ -0,0 +1,22 @@
+package pack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Google {
+	@Test
+	public void test(){
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.close();
+			
+		
+	}
+}
